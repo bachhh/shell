360b6a06f9dad85257ce36673bb0dc8eb5e40770
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2016-11-29 23:01:19 +0000

    test

--- fun.c
+++ fun.c
@@ -4,42 +4,10 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
-char* directoryPrompt;
-
-int main(int argc, char **argv){
-  int status;
-  char* inputLine;
-  char** cmdAndArgs;
-
-
-  set_prompt(directoryPrompt);
-  while{
-      printf("%s>", directoryPrompt);
-      inputLine = read_input();
-      cmd = parse_input(inputLine);
-      status = launch_cmd(cmd);
-  }
-
-  free(directoryPrompt);
-  return 0;
-}
-
-#define PROMPT_SIZE = 500
-void set_prompt(char * buf){
-  buf = malloc(sizeof(char) * PROMPT_SIZE);
-  int promptSizeMultiplier = 1;
-  // Dynamic memory alloc
-  while(getcwd(directoryPrompt, PROMPT_SIZE) == NULL){
-    i++;
-    directoryPrompt = realloc(directoryPrompt, PROMPT_SIZE * promptSizeMultiplier);
-  }
-}
-
-#define INPUT_BUFFER_SIZE 500
+#define INPUT_BUFFER_SIZE 2038 
 char* read_input(){
   char * buffer = malloc(sizeof(char)*INPUT_BUFFER_SIZE);
   size_t size;
-  // Getline already does dynamic memory management
   if (getline(&buffer, &size, stdin) == -1){
     printf("Error getline input");
   }
@@ -47,8 +15,8 @@ char* read_input(){
 }
 
 
-#define POSIX_SHELL_TOKEN_DELIM = " \t\n"
-#define COMMAND_BUFFER_SIZE = 500
+#define POSIX_SHELL_TOKEN_DELIM " \t\n"
+#define COMMAND_BUFFER_SIZE 1024 
 char** parse_input(char* string){
   char** buffer = malloc(sizeof(char)*COMMAND_BUFFER_SIZE);
   int bufferPosition = 0;
@@ -67,4 +35,49 @@ char** parse_input(char* string){
   }
   buffer[bufferPosition] = NULL;
   return buffer;
+}
+
+
+int launch_cmd(char** cmdAndArgs){
+  pid_t children_pid;
+  int status;
+  children_pid = fork();
+  if (children_pid == 0){
+    execvp(cmdAndArgs[0], cmdAndArgs); 
+  }
+  else {
+    wait(NULL);
+  }
+  return status;
+}
+
+
+
+
+#define PROMPT_SIZE 500
+char* directoryPrompt;
+
+int main(int argc, char **argv){
+  char* inputLine;
+  char** cmdAndArgs;
+  int status;
+  
+  while(1){
+      directoryPrompt = malloc(sizeof(char) * PROMPT_SIZE);
+      getcwd(directoryPrompt, PROMPT_SIZE); 
+      printf("%s>", directoryPrompt);
+      inputLine = read_input();
+      cmdAndArgs = parse_input(inputLine);
+      if(strcmp(cmdAndArgs[0], "cd" ) == 0){
+        launch_cd(cmdAndArgs);
+      }
+      else if (strcmp(cmdAndArgs[0], "exit") == 0){
+        break;
+      }
+      else { 
+        chdir(cmdAndArgs[1]);
+      }
+      free(directoryPrompt);
+  }
+  return 0;
 }
